78d7584fd102e54156f7c859170a02b72578e0bf
test <sebastian.kuppa@web.de>
2023-02-01 18:38:29 +0800

    added update() method to BoxesGame class

--- boxes.py
+++ boxes.py
@@ -10,3 +10,17 @@ class BoxesGame:
         pygame.display.set_caption('Boxes Multiplayer Game')
         # init clock
         self.clock = pygame.time.Clock()
+
+    def update(self):
+        # sleep function
+        self.clock.tick(60)
+        # clear screen
+        self.window.fill(0)
+
+        for event in pygame.event.get():
+            # exit game when quit button is pressed
+            if event.type == pygame.QUIT:
+                exit()
+        # update the window screen
+        pygame.display.flip()
+
