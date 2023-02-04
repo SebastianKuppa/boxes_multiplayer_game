c13a6a4e1003695185d7edceb784ad0d1adcb70d
test <sebastian.kuppa@web.de>
2023-02-04 16:11:57 +0800

    debugging boardgame bars

--- boxes.py
+++ boxes.py
@@ -4,7 +4,7 @@ import pygame
 class BoxesGame:
     def __init__(self):
         pygame.init()
-        bounds = (389, 489)
+        bounds = (400, 489)
         # init window
         self.window = pygame.display.set_mode(bounds,)
         pygame.display.set_caption('Boxes Multiplayer Game')
@@ -28,14 +28,14 @@ class BoxesGame:
     def drawBoard(self):
         for x in range(6):
             for y in range(7):
-                if not self.boardh[x][y]:
-                    self.window.blit(self.normal_line_h, [x*64+5, y*64])
+                if not self.boardh[y][x]:
+                    self.window.blit(self.normal_line_v, [x*64+5, y*64])
                 else:
                     self.window.blit(self.bar_h, [x * 64 + 5, y * 64])
         for x in range(7):
             for y in range(6):
-                if not self.boardv[x][y]:
-                    self.window.blit(self.normal_line_v, [x * 64 + 5, y * 64])
+                if not self.boardv[y][x]:
+                    self.window.blit(self.normal_line_h, [x * 64 + 5, y * 64])
                 else:
                     self.window.blit(self.bar_v, [x * 64 + 5, y * 64])
 
