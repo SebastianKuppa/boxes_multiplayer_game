b25d4e71c0c36434dbb749db74f756ce0dfaf041
sk <sebastian.kuppa@web.de>
2023-03-01 13:28:56 +0800

    added Game class with init function on server.py

--- server.py
+++ server.py
@@ -16,6 +16,22 @@ class BoxesServer(PodSixNet.Server.Server):
         print('new connection: ', channel)
 
 
+class Game:
+    def __init__(self, player0, currentindex):
+        # whos turn is it 1 or 0
+        self.turn = 0
+        # owner map
+        self.owner = [[False for x in range(6)] for y in range(6)]
+        # vertical and horizontal boards of bars
+        self.boardh = [[False for x in range(6)] for y in range(7)]
+        self.boardv = [[False for x in range(7)] for y in range(6)]
+        # init players
+        self.player0 = player0
+        self.player1 = None
+        # init gameID
+        self.gameid = currentindex
+
+
 print("STARTING SERVER ON LOCALHOST.")
 boxesServe = BoxesServer()
 while True:
